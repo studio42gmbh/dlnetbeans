0c510a34d1750b5509d733fb06264bc0b194944c
Benjamin Schiller <bs@s42m.de>
2023-01-03 23:54:34 +0100

    First semi stable version of the indenter

--- src/main/java/de/s42/dl/netbeans/indentation/DLIndenter.java
+++ src/main/java/de/s42/dl/netbeans/indentation/DLIndenter.java
@@ -37,6 +37,7 @@ import org.antlr.v4.runtime.TokenStream;
 import org.netbeans.editor.BaseDocument;
 import org.netbeans.modules.editor.indent.api.IndentUtils;
 import org.netbeans.modules.editor.indent.spi.Context;
+import org.netbeans.modules.editor.indent.spi.Context.Region;
 
 /**
  *
@@ -45,12 +46,16 @@ import org.netbeans.modules.editor.indent.spi.Context;
 public class DLIndenter
 {
 
+	public final static int MAX_LINE_LOOKAHEAD = 1000;
+
 	private final static Logger log = LogManager.getLogger(DLIndenter.class.getName());
 
 	protected final Context context;
 
 	public DLIndenter(Context context)
 	{
+		assert context != null;
+		
 		this.context = context;
 	}
 
@@ -61,72 +66,117 @@ public class DLIndenter
 
 	public void reindent() throws BadLocationException
 	{
-		BaseDocument document = getDocument();
-
-		//log.warn("reindent", context.caretOffset(), context.startOffset(), context.endOffset(), document.getStartPosition().getOffset(), document.getEndPosition().getOffset());
-		// Do a complete reindent
-		if (context.startOffset() == document.getStartPosition().getOffset()
-			&& context.endOffset() == document.getEndPosition().getOffset() - 1) {
-			completeReindent();
+		for (Region region : context.indentRegions()) {
+			reindentRegion(region.getStartOffset(), region.getEndOffset());
 		}
 	}
 
-	public void completeReindent() throws BadLocationException
+	public void reindentRegion(int startIndex, int endIndex) throws BadLocationException
 	{
-		// Get the text safely
-		BaseDocument document = getDocument();
+		assert startIndex >= 0;
+		assert endIndex >= 0;
+		assert endIndex >= startIndex;
 
+		//log.debug("reindent", startIndex, endIndex);
+		BaseDocument document = getDocument();
 		String text = FileObjectHelper.getText(document);
 		int indentLevel = IndentUtils.indentLevelSize(document);
-
-		// Prepare the lexer
-		DLLexer lexer = new DLLexer(CharStreams.fromString(text));
-		lexer.removeErrorListeners();
-		TokenStream tokens = new BufferedTokenStream(lexer);
+		TokenStream tokens = getDLTokenStream(text);
 
 		// Iterate tokens from lexer
-		StringBuilder builder = new StringBuilder(text.length() * 3 / 2);
-		boolean justNewline = false;
+		boolean justNewline = true;
 		int indent = 0;
+		int offset = 0;
+		int adjustedEndIndex = endIndex;
 		while (true) {
 			Token token = tokens.LT(1);
-			Token nextToken = tokens.LT(2);
 
-			//log.info("TOKEN : " + token);
-			// End of file
-			if (token.getType() == DLLexer.EOF) {
+			// End of file or behind of indent region
+			if (token.getType() == DLLexer.EOF
+				|| offset > adjustedEndIndex) {
 				break;
 			}
 
-			// Reduce indentation on }
-			if (nextToken.getType() == DLLexer.SCOPE_CLOSE) {
-				indent = Math.max(0, indent - 1);
-			}
-			
-			// If there was a new line before and it is not immediately followed by another newline -> add indent
-			if (justNewline && nextToken.getType() != DLLexer.NEWLINE) {
-				builder.append(IndentUtils.createIndentString(document, indent * indentLevel));
+			// Just indent in given region
+			if (justNewline && offset >= startIndex) {
+
+				int adjustedIndent = Math.max(0, indent - countDirectClosingScopesInThisLine(tokens));
+
+				int lineOff = context.lineStartOffset(offset);
+				int newLineIndent = adjustedIndent * indentLevel;
+				//int lineIndent = context.lineIndent(lineOff);
+				//log.warn("justNewline", lineIndent, newLineIndent);
+				context.modifyIndent(lineOff, newLineIndent);
+
+				// Adjust for added whitespaces
+				int oldLineIndenInChars = IndentUtils.createIndentString(document, newLineIndent).length();
+				offset += oldLineIndenInChars;
+				adjustedEndIndex += oldLineIndenInChars;
+
+				// Adjust for removed whitespaces
+				if (token.getType() == DLLexer.WHITESPACES) {
+					offset -= token.getText().length();
+					adjustedEndIndex -= token.getText().length();
+				}
 			}
 
 			// Increase indentation on {
 			if (token.getType() == DLLexer.SCOPE_OPEN) {
 				indent += 1;
+			} // Reduce indentation on }
+			else if (token.getType() == DLLexer.SCOPE_CLOSE) {
+				indent = Math.max(0, indent - 1);
 			}
-
-			// If it is not newline or a whitepace -> Just add the content
-			if (!justNewline || token.getType() != DLLexer.WHITESPACES) {
-				builder.append(token.getText());
-			}
-
-			// Newline sets justNewline for next iteration
-			justNewline = (token.getType() == DLLexer.NEWLINE);
 
 			// Proceed one token
+			offset += token.getText().length();
+			justNewline = (token.getType() == DLLexer.NEWLINE);
 			tokens.consume();
 		}
+	}
 
-		// Replace the whole text safely
-		FileObjectHelper.replaceText(document, builder.toString());
+	/**
+	 * Constructs a new TokenStream with an underlying DLLexer from a given text
+	 * @param content
+	 * @return 
+	 */
+	protected TokenStream getDLTokenStream(String content)
+	{
+		assert content != null;
+
+		DLLexer lexer = new DLLexer(CharStreams.fromString(content));
+		lexer.removeErrorListeners();
+
+		return new BufferedTokenStream(lexer);
+	}
+
+	/**
+	 * Return the number of direct closing scopes from the beginning of a line. anything else but whitespaces ends the count
+	 * @param tokens
+	 * @return 
+	 */
+	protected int countDirectClosingScopesInThisLine(TokenStream tokens)
+	{
+		assert tokens != null;
+
+		int count = 0;
+
+		for (int i = 1; i < MAX_LINE_LOOKAHEAD; ++i) {
+
+			Token token = tokens.LT(i);
+
+			switch (token.getType()) {
+				case DLLexer.WHITESPACES:
+					break;
+				case DLLexer.SCOPE_CLOSE:
+					count += 1;
+					break;
+				default:
+					return count;
+			}
+		}
+
+		return count;
 	}
 
 	// <editor-fold desc="Getters/Setters" defaultstate="collapsed">
